142aeaa819272fbe53348751dfeb20acc9d52bd8
A3Tom <Tom_Esq@Yahoo.com>
2023-01-24 01:59:37 +0000

    Slight refactor. Not a fan of denoting strings as nullable

--- DichotomicSearch.Application/Models/MorseCode.cs
+++ DichotomicSearch.Application/Models/MorseCode.cs
@@ -8,16 +8,4 @@ public class MorseCode
         public const char DASH_SYMBOL = '-';
         public const char UNKNOWN_SYMBOL = '?';
     }
-
-    public static HashSet<Node> Tree =>
-        new()
-        {
-                new Node(string.Empty, "E", "T"),
-                new Node("E", "I", "A"),
-                new Node("T", "N", "M"),
-                new Node("I", "S", "U"),
-                new Node("A", "R", "W"),
-                new Node("N", "D", "K"),
-                new Node("M", "G", "O")
-        };
 }
--- DichotomicSearch.Application/Models/Node.cs
+++ DichotomicSearch.Application/Models/Node.cs
@@ -2,14 +2,7 @@
 
 public class Node
 {
-    public Node(string parent, string left, string right)
-    {
-        Parent = parent;
-        Left = left;
-        Right = right;
-    }
-
-    public readonly string Parent;
-    public readonly string Left;
-    public readonly string Right;
+    public string? Parent { get; init; }
+    public string? Left { get; init; }
+    public string? Right { get; init; }
 }
