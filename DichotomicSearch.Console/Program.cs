2d9ac98fcd80b57e9a43534b88c32ffc735feee7
A3Tom <Tom_Esq@Yahoo.com>
2023-01-24 01:59:20 +0000

    Slightly overengineered this, but it was fun to write

--- DichotomicSearch.Console/Program.cs
+++ DichotomicSearch.Console/Program.cs
@@ -1,11 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DichotomicSearch.Application.Models;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+const string ROOT_DIR_NAME = "dichotomic-search";
+const string MORSE_CODE_TREE_RELATIVE_PATH = @"resources/morse-coded-tree.json";
 
 Console.WriteLine("Hello, World!");
 
+var fileName = GetFilePath();
 
-var hing = MorseCode.Tree.ToDictionary(k => k.Parent, v => new { v.Left, v.Right });
-File.WriteAllText("whatever.json", System.Text.Json.JsonSerializer.Serialize(hing));
+string jsonString = File.ReadAllText(fileName, Encoding.UTF8);
+var morseCodeTree = JsonSerializer.Deserialize<HashSet<Node>>(jsonString)!;
+
+foreach (var node in morseCodeTree)
+    Console.WriteLine($"[{node.Parent}] : {node.Left} | {node.Right}");
+
+Console.WriteLine("Loaded morse code tree!");
 
 
-Console.WriteLine("Done!");
+
+static string GetFilePath()
+{
+    var runLocation = Assembly.GetExecutingAssembly().Location;
+    var folderLocationIdx = runLocation.IndexOf(ROOT_DIR_NAME);
+    return $"{runLocation[..(folderLocationIdx + ROOT_DIR_NAME.Length)]}/{MORSE_CODE_TREE_RELATIVE_PATH}";
+}
