1e0a1392c1cfc3eadf36441665f1e8d310bdc32e
A3Tom <Tom_Esq@Yahoo.com>
2023-01-29 00:28:46 +0000

    Added suspiciously specific code to load a morse code tree

---
+++ DichotomicSearch.Application/Classes/MorseCodeTreeLoader.cs
@@ -0,0 +1,24 @@
+﻿using DichotomicSearch.Application.Models;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+namespace DichotomicSearch.Application.Classes;
+public class MorseCodeTreeBuilder
+{
+    // Well ain't this specific ...
+    public static HashSet<Node> LoadFileFromRelativePath()
+    {
+        var fileName = GetFilePath();
+
+        string jsonString = File.ReadAllText(fileName, Encoding.UTF8);
+        return JsonSerializer.Deserialize<HashSet<Node>>(jsonString)!;
+    }
+
+    private static string GetFilePath()
+    {
+        var runLocation = Assembly.GetExecutingAssembly().Location;
+        var folderLocationIdx = runLocation.IndexOf(Settings.ROOT_DIR_NAME);
+        return $"{runLocation[..(folderLocationIdx + Settings.ROOT_DIR_NAME.Length)]}/{Settings.MORSE_CODE_TREE_RELATIVE_PATH}";
+    }
+}
