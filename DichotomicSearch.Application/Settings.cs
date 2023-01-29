e581d3958ce50a9fd6b2935e1623f6fc9260bf02
A3Tom <Tom_Esq@Yahoo.com>
2023-01-29 00:29:02 +0000

    Added a new settings static class

---
+++ DichotomicSearch.Application/Settings.cs
@@ -0,0 +1,6 @@
+﻿namespace DichotomicSearch.Application;
+public static class Settings
+{
+    public const string ROOT_DIR_NAME = "dichotomic-search";
+    public const string MORSE_CODE_TREE_RELATIVE_PATH = "resources/morse-code-tree.json";
+}
