1ffb89890497c78a19a46b4459e8d6fccb086033
A3Tom <Tom_Esq@Yahoo.com>
2023-01-29 00:28:25 +0000

    Renamed Search class to SearchService Refactored this a bit too to be a tad neater

---
+++ DichotomicSearch.Application/Classes/SearchService.cs
@@ -0,0 +1,49 @@
+﻿using DichotomicSearch.Application.Models;
+
+namespace DichotomicSearch.Application.Classes;
+public class SearchService
+{
+    public SearchService(ICollection<Node> morseCodeNodes)
+    {
+        _morseCodeNodes = morseCodeNodes;
+    }
+
+    private readonly ICollection<Node> _morseCodeNodes;
+
+    public string[] TransformSymbolsToNodes(string signals)
+    {
+        var previousNodePossibilities = new string[] { string.Empty };
+
+        for (int i = 0; i < signals.Length; i++)
+        {
+            var validNodes = GetValidNodes(_morseCodeNodes, previousNodePossibilities);
+            var currentNodePossibilities = new List<string>();
+
+            foreach (var node in validNodes)
+            {
+                if (DestinationNodeIsNotEmpty(node.Left) && IsDotOrUnkown(signals[i]))
+                    currentNodePossibilities.Add(node.Left!);
+
+                if (DestinationNodeIsNotEmpty(node.Right) && IsDashOrUnkown(signals[i]))
+                    currentNodePossibilities.Add(node.Right!);
+            }
+            previousNodePossibilities = currentNodePossibilities.ToArray();
+        }
+
+        return previousNodePossibilities.ToArray();
+    }
+
+    private static bool IsDotOrUnkown(char signal) =>
+        signal == MorseCode.Symbols.DOT_SYMBOL ||
+        signal == MorseCode.Symbols.UNKNOWN_SYMBOL;
+
+    private static bool IsDashOrUnkown(char signal) =>
+        signal == MorseCode.Symbols.DASH_SYMBOL ||
+        signal == MorseCode.Symbols.UNKNOWN_SYMBOL;
+
+    private static bool DestinationNodeIsNotEmpty(string? destination) => 
+        !string.IsNullOrEmpty(destination);
+
+    private static IEnumerable<Node> GetValidNodes(ICollection<Node> morseCodeTree, string[] parents) => 
+        morseCodeTree.Where(x => parents.Contains(x.Parent));
+}
