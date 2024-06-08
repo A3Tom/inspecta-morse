fbc45e61ec19b802a3debc4ce885e3f99f61b106
Tom Smith <Tom_Esq@Yahoo.com>
2024-06-08 21:43:38 +0100

    Might just fuck up some yaml, who knows

--- DichotomicSearch.Application/Classes/MorseCodeTreeLoader.cs
+++ DichotomicSearch.Application/Classes/MorseCodeTreeLoader.cs
@@ -2,23 +2,42 @@
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
+using YamlDotNet.Serialization;
 
 namespace DichotomicSearch.Application.Classes;
 public class NodeTreeBuilder
 {
-    // Well ain't this specific ...
-    public static HashSet<Node> LoadFileFromRelativePath()
+    public static HashSet<Node> LoadYamlFileFromRelativePath(string relativePath)
     {
-        var fileName = GetFilePath();
+        var fullFilePath = $"{GetBaseFilePath()}/{relativePath}";
+        
+        string yamlString = File.ReadAllText(fullFilePath, Encoding.UTF8);
+        var yamlDeserializer = new DeserializerBuilder().Build();
+
+        return yamlDeserializer.Deserialize<HashSet<Node>>(yamlString);
+    }
+
+    public static void SaveNodeTreeAsYamlFile(HashSet<Node> nodes, string filename)
+    {
+        var yamlSerializer = new SerializerBuilder().Build();
+        var outputYaml = yamlSerializer.Serialize(nodes);
+
+        var filepath = $"{GetBaseFilePath()}/{filename}.yaml";
+        File.AppendAllText(filepath, outputYaml);
+    }
+
+    public static HashSet<Node> LoadJsonFileFromRelativePath(string relativePath)
+    {
+        var fileName = $"{GetBaseFilePath()}/{relativePath}";
 
         string jsonString = File.ReadAllText(fileName, Encoding.UTF8);
         return JsonSerializer.Deserialize<HashSet<Node>>(jsonString)!;
     }
 
-    private static string GetFilePath()
+    private static string GetBaseFilePath()
     {
         var runLocation = Assembly.GetExecutingAssembly().Location;
         var folderLocationIdx = runLocation.IndexOf(Settings.ROOT_DIR_NAME);
-        return $"{runLocation[..(folderLocationIdx + Settings.ROOT_DIR_NAME.Length)]}/{Settings.MORSE_CODE_TREE_RELATIVE_PATH}";
+        return $"{runLocation[..(folderLocationIdx + Settings.ROOT_DIR_NAME.Length)]}";
     }
 }
--- DichotomicSearch.Application/Settings.cs
+++ DichotomicSearch.Application/Settings.cs
@@ -3,4 +3,5 @@ public static class Settings
 {
     public const string ROOT_DIR_NAME = "dichotomic-search";
     public const string MORSE_CODE_TREE_RELATIVE_PATH = "resources/morse-code-tree.json";
+    public const string TEST_HUFFMAN_TREE_RELATIVE_PATH = "resources/test-huffman-tree.yaml";
 }
