b742338bf0b6dced28b598c8c3c57cc1082ff563
Tom Smith <Tom_Esq@Yahoo.com>
2024-06-08 21:43:54 +0100

    Refactored the mess of program.cs in to a longer file name

---
+++ DichotomicSearch.Console/HuffmanEncodingProgram.cs
@@ -0,0 +1,13 @@
+﻿using DichotomicSearch.Application.Classes;
+using DichotomicSearch.Application;
+
+namespace dichotomic_search;
+public class HuffmanEncodingProgram
+{
+    public static void Run()
+    {
+        var nodeTree = NodeTreeBuilder.LoadYamlFileFromRelativePath(Settings.TEST_HUFFMAN_TREE_RELATIVE_PATH);
+
+
+    }
+}
