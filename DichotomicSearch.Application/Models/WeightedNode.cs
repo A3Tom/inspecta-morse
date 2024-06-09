0df3c268daca856114a6b891b64d5a02de87a832
Tom Smith <Tom_Esq@Yahoo.com>
2024-06-09 12:26:18 +0100

    Added weighted node

---
+++ DichotomicSearch.Application/Models/WeightedNode.cs
@@ -0,0 +1,20 @@
+﻿namespace DichotomicSearch.Application.Models;
+public class WeightedNode : Node
+{
+    public int Weighting { get; init; }
+
+    public WeightedNode(string key, int weighting)
+    {
+        Key = key;
+        Weighting = weighting;
+    }
+
+    public WeightedNode(WeightedNode leftNode, WeightedNode rightNode)
+    {
+        Key = leftNode.Key + rightNode.Key;
+        Weighting = leftNode.Weighting + rightNode.Weighting;
+
+        Left = leftNode.Key;
+        Right = rightNode.Key;
+    }
+}
