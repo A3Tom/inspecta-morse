5f5a658ea1a48ef5fdbdf1f373d0e20522e891fa
Tom Smith <Tom_Esq@Yahoo.com>
2024-06-10 23:14:20 +0100

    Added com port keyer !

--- DichotomicSearch.Application/Settings.cs
+++ DichotomicSearch.Application/Settings.cs
@@ -4,4 +4,6 @@ public static class Settings
     public const string ROOT_DIR_NAME = "dichotomic-search";
     public const string MORSE_CODE_TREE_RELATIVE_PATH = "resources/morse-code-tree";
     public const string TEST_HUFFMAN_TREE_RELATIVE_PATH = "resources/test-huffman-tree";
+
+    public const string MORSE_KEYER_COM_PORT = "COM4";
 }
---
+++ DichotomicSearch.Console/COMPortKeyer.cs
@@ -0,0 +1,35 @@
+﻿using DichotomicSearch.Application.Classes.Implementations;
+using DichotomicSearch.Application.Classes;
+using DichotomicSearch.Application.Models;
+using System.IO.Ports;
+using DichotomicSearch.Application;
+
+namespace dichotomic_search;
+public class COMPortKeyer
+{
+    private static readonly HashSet<Node> _morseCodeNodes = NodeTreeBuilder.LoadFileFromRelativePath(Settings.MORSE_CODE_TREE_RELATIVE_PATH, FileType.YAML);
+
+    private static readonly SearchTraversalService _morseSearchService = new(_morseCodeNodes);
+    private static readonly SerialPort _morseKeyerSerialPort = new(Settings.MORSE_KEYER_COM_PORT);
+
+    public static void Run()
+    {
+        if (!_morseKeyerSerialPort.IsOpen)
+            _morseKeyerSerialPort.Open();
+
+        while (true)
+        {
+            while (_morseKeyerSerialPort.BytesToRead > 0)
+            {
+                var incoming = _morseKeyerSerialPort.ReadLine();
+                if (incoming.StartsWith("Morse Message : "))
+                {
+                    var colonidx = incoming.IndexOf(":") + 1;
+                    var morseCode = incoming.Replace("\r", "")[colonidx..].Trim();
+                    var output = _morseSearchService.TransformSymbolsToNodes(morseCode);
+                    Console.WriteLine($"Morse : {morseCode} | Translated : {string.Join("", output)}");
+                }
+            }
+        }
+    }
+}
