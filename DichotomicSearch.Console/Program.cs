bae06cc483d49e674949452d4cdfca442bba279c
Tom Smith <Tom_Esq@Yahoo.com>
2024-06-11 19:31:32 +0100

    Switched program.cs to run COM port program by default

--- DichotomicSearch.Console/Program.cs
+++ DichotomicSearch.Console/Program.cs
@@ -1,5 +1,10 @@
 ﻿using dichotomic_search;
+using DichotomicSearch.Application.Classes.Implementations;
+using DichotomicSearch.Application.Classes;
+using DichotomicSearch.Application.Models;
+using System.IO.Ports;
+using DichotomicSearch.Application;
 
+COMPortKeyer.Run();
 //MorseCodeProgram.Run();
-
-HuffmanEncodingProgram.Run();
+//HuffmanEncodingProgram.Run();
