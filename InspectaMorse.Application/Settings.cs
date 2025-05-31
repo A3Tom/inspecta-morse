5ff73f715ee90ef0c50329b1d8a8e8ad303c5f2e
Tom Smith <Tom_Esq@Yahoo.com>
2025-05-31 17:50:56 +0100

    Updated settings

--- InspectaMorse.Application/Settings.cs
+++ InspectaMorse.Application/Settings.cs
@@ -1,9 +1,9 @@
 ﻿namespace InspectaMorse.Application;
 public static class Settings
 {
-    public const string ROOT_DIR_NAME = "dichotomic-search";
+    public const string ROOT_DIR_NAME = "inspecta-morse";
     public const string MORSE_CODE_TREE_RELATIVE_PATH = "resources/morse-code-tree";
     public const string TEST_HUFFMAN_TREE_RELATIVE_PATH = "resources/test-huffman-tree";
 
-    public const string MORSE_KEYER_COM_PORT = "COM4";
+    public const string MORSE_KEYER_COM_PORT = "COM5";
 }
